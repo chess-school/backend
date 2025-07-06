d3b774fbfd80ac2a29ba9bfbe024dd0e6967fff1
NikitaSemenukha <skarabaychick@gmail.com>
2025-07-06 17:22:32 +0200

    fix

--- server.js
+++ server.js
@@ -11,10 +11,18 @@ connectDB();
 const app = express();
 const server = http.createServer(app);
 
+const originLinks = [
+    'http://localhost:3002', 
+    'http://localhost:3001', 
+    'http://localhost:5173', 
+    'https://new-front-u2qi-git-dev-nikitas-projects-27f00a22.vercel.app', 
+    'https://new-front-u2qi-93wo39vf6-nikitas-projects-27f00a22.vercel.app'
+];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['http://localhost:3002', 'http://localhost:3001', 'http://localhost:5173', 'https://new-front-u2qi-git-dev-nikitas-projects-27f00a22.vercel.app', 'https://new-front-u2qi-frbvki4id-nikitas-projects-27f00a22.vercel.app'],
+    origin: originLinks,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
